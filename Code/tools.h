26ad332d937a614f0d80afb2c55801bae1f5f589
root <root@DESKTOP.localdomain>
2022-10-07 20:48:04 +0400

    first try upgrade

---
+++ Code/tools.h
@@ -0,0 +1,32 @@
+#ifndef TOOLS_HPP
+#define TOOLS_HPP
+
+
+
+//TOOLbar
+
+
+
+//add picture
+//add text
+//add objects 
+    //cirquit
+    //arrow
+    //line
+    //...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif !TOOLS_HPP
